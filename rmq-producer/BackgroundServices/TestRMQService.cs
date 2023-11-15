01093291d151ed9aaa6583fd5794f4475ebf7a04
Ador-25 <abtahee.ador25@gmail.com>
2023-11-15 17:19:06 +0600

    initial commit - rmq producer added in background service

---
+++ rmq-producer/BackgroundServices/TestRMQService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using rmq_producer.Models;
+using System.Text;
+
+
+namespace rmq_producer.BackgroundServices
+{
+    public class TestRMQService : BackgroundService
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IConfiguration _configuration;
+        public TestRMQService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionFactory = new ConnectionFactory() { HostName = _configuration["RMQ_Host"], UserName = _configuration["RMQ_User"], Password = _configuration["RMQ_Pass"] };
+            _connection = _connectionFactory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(queue: "ador-test-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        }
+        public void SendMessage(Order order)
+        {
+            string message = JsonConvert.SerializeObject(order);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            // Publish message to the queue
+            _channel.BasicPublish(exchange: "", routingKey: "ador-test-queue", basicProperties: null, body: body);
+            Console.WriteLine($" [x] Sent \n {message}");
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Simulate some background processing
+                await Task.Delay(5000, stoppingToken);
+
+                // Publish a sample message to the queue
+
+                SendMessage(new Order { Id = new Random().Next(10), Description = "Test string", Amount = 50.00, IsCompleted = new Random().Next(10) % 2 == 0 });
+            }
+        }
+    }
+}
---
+++ rmq-producer/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace rmq_producer.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
